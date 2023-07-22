ffdbd66a384b30aa265eabde5a07e9c1742d013f
WWG0824 <115329580+WWG0824@users.noreply.github.com>
2023-07-22 14:44:46 +0800

    Update index.js

--- config/index.js
+++ config/index.js
@@ -1,10 +1,10 @@
 module.exports = {
-  appid: "wx71941f6106ae1072",
-  appsecret: "b4b2ad8a8d4a94e403831886ac10c646",
-  template_id: "q14lTqsDyK0N-KcZUs8UKHOl3GWn2uErDk-8hbm2ouQ",
-  touser: "oonWJ5slWuS2OrJdgCLM3GmNdxVk",
+  appid: "wxbb37ac5b0fcb14f8",
+  appsecret: "8578a1d830b05844c3409e21b9e7dd6d",
+  template_id: "9vokW5xjdj_CGHmBJdjReSkMhg24QrHRShY9bMhLNKw",
+  touser: "ory6f6UOaX2u6dpQcZY8FhWtHr0I",
 
   key: "201c93c2e2b34bdeecdbf9f5265eb84e",
-  city: "邢台",
-  first_date: "2022-11-10",
+  city: "临汾",
+  first_date: "2022-10-27",
 };
